28fa43bb0c092b65734d3c2742244d14b1c3f3e1
Markuss Birznieks <piebmarkuss@gmail.com>
2024-10-22 20:53:18 +0300

    Added automatic migrations.

---
+++ Data/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MqqtConsumer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MqttConsumer.Data;
+
+public static class Extensions
+{
+    public static void CreateDbIfNotExists(this IHost host)
+    {
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<SensorDbContext>();
+                context.Database.Migrate();
+                //DbInitializer.Initialize(context);
+            }
+        }
+    }
+}
